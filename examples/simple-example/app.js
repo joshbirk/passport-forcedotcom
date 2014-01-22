6b4901c8f780fd32c129d87f62043c7a372d8a86
Jeremy Stashewsky <jeremy@goinstant.com>
2014-01-21 17:01:29 -0800

    Clean up example app

--- examples/simple-example/app.js
+++ examples/simple-example/app.js
@@ -4,31 +4,39 @@ var express = require('express'),
   ForceDotComStrategy = require('passport-forcedotcom').Strategy;
   
 
-//----------------------------------------------------------------------------------------------------------------------
-// ---------- REPLACE THE BELOW SETTING TO MATCH YOUR SALESFORCE CONNECTED-APP'S SETTINGS ------------------------------
-//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+// REPLACE THE BELOW SETTING TO MATCH YOUR SALESFORCE CONNECTED-APP'S SETTINGS
+//----------------------------------------------------------------------------
 
-//Set Force.com app's clientID
+// Set Force.com app's clientID
 var CF_CLIENT_ID = '3MVG9A2kN3Bn17hsWsLDatw._IRRcBapWFgecAzRUqAny5.wuHmAMejzvV7ZhFlTg5ZPNdHBDjS18Zu0cvgeN';
 
-//Set Force.com app's clientSecret
+// Set Force.com app's clientSecret
 var CF_CLIENT_SECRET = '3585278186716093184';
 
-// Note: You should have a app.get(..) for this callback to receive callback from Force.com\
+// Note: You should have a app.get(..) for this callback to receive callback
+// from Force.com
 //
-// For example: If your callback url is: https://localhost:3000//auth/forcedotcom/callback'
-// then, you should have a HTTP GET endpoint like: app.get('/auth/forcedotcom/callback, callback))
+// For example, if your callback url is:
+//
+//   https://localhost:3000/auth/forcedotcom/callback
+// 
+// then, you should have a HTTP GET endpoint like:
+//
+//   app.get('/auth/forcedotcom/callback, callback))
 //
 var CF_CALLBACK_URL = 'http://localhost:3000/auth/forcedotcom/callback';
 
 
-// Salesforce Authorization URL (typically this is: https://login.salesforce.com/services/oauth2/authorize)
+// Salesforce Authorization URL (this defaults to:
+// https://login.salesforce.com/services/oauth2/authorize)
 var SF_AUTHORIZE_URL = 'https://login.salesforce.com/services/oauth2/authorize';
 
-//Salesforce token URL (typically this is: https://login.salesforce.com/services/oauth2/token)
+// Salesforce token URL (this defaults to:
+// https://login.salesforce.com/services/oauth2/token)
 var SF_TOKEN_URL = 'https://login.salesforce.com/services/oauth2/token';
 
-//---------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 
 // Passport session setup.
@@ -39,11 +47,11 @@ var SF_TOKEN_URL = 'https://login.salesforce.com/services/oauth2/token';
 //   have a database of user records, the complete Salesforce profile is
 //   serialized and deserialized.
 passport.serializeUser(function(user, done) {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-    done(null, obj);
+  done(null, obj);
 });
 
 
@@ -51,25 +59,23 @@ passport.deserializeUser(function(obj, done) {
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Salesforce
 //   profile), and invoke a callback with a user object.
-
-
 var sfStrategy = new ForceDotComStrategy({
-    clientID: CF_CLIENT_ID,
-    clientSecret: CF_CLIENT_SECRET,
-    callbackURL: CF_CALLBACK_URL,
-    authorizationURL: SF_AUTHORIZE_URL,
-    tokenURL: SF_TOKEN_URL
+  clientID: CF_CLIENT_ID,
+  clientSecret: CF_CLIENT_SECRET,
+  callbackURL: CF_CALLBACK_URL,
+  authorizationURL: SF_AUTHORIZE_URL,
+  tokenURL: SF_TOKEN_URL
 }, function(accessToken, refreshToken, profile, done) {
 
-    // asynchronous verification, for effect...
-    process.nextTick(function() {
+  // asynchronous verification, for effect...
+  process.nextTick(function() {
 
-        // To keep the example simple, the user's forcedotcom profile is returned to
-        // represent the logged-in user.  In a typical application, you would want
-        // to associate the forcedotcom account with a user record in your database,
-        // and return that user instead.
-        return done(null, profile);
-    });
+    // To keep the example simple, the user's forcedotcom profile is returned to
+    // represent the logged-in user.  In a typical application, you would want
+    // to associate the forcedotcom account with a user record in your database,
+    // and return that user instead.
+    return done(null, profile);
+  });
 });
 
 passport.use(sfStrategy);
@@ -79,57 +85,55 @@ var app = express();
 
 // configure Express
 app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.logger());
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.session({
-        secret: 'keyboard cat'
-    }));
-    // Initialize Passport!  Also use passport.session() middleware, to support
-    // persistent login sessions (recommended).
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({
+    secret: 'keyboard cat'
+  }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 
 app.get('/', function(req, res) {
-    console.log(req.user);
-    console.log(req._oauthData);
-    if(!req.user) {
-        req.session.destroy();
-        req.logout();
-        //sfStrategy.reset();
-        return res.redirect('/login');
-    }
-    res.render('index', {
-        user: req.user
-    });
+  console.log(req.user);
+  console.log(req._oauthData);
+  if(!req.user) {
+    req.session.destroy();
+    req.logout();
+    return res.redirect('/login');
+  }
+  res.render('index', {
+    user: req.user
+  });
 });
 
 
 app.get('/login', function(req, res) {
-    req.session.destroy();
-    req.logout();
-    //sfStrategy.reset(); //reset auth tokens
+  req.logout();
+  req.session.destroy();
 
-    res.render('login', {
-        user: req.user
-    });
+  res.render('login', {
+    user: req.user
+  });
 });
 
 // GET /auth/forcedotcom
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Force.com authentication will involve
-//   redirecting the user to your domain.  After authorization, Force.com
-//   will redirect the user back to this application at /auth/forcedotcom/callback
+//   redirecting the user to your domain.  After authorization, Force.com will
+//   redirect the user back to this application at /auth/forcedotcom/callback
 app.get('/auth/forcedotcom', passport.authenticate('forcedotcom'), function(req, res) {
-    // The request will be redirected to Force.com for authentication, so this
-    // function will not be called.
+  // The request will be redirected to Force.com for authentication, so this
+  // function will not be called.
 });
 
 // GET /auth/forcedotcom/callback
@@ -139,13 +143,13 @@ app.get('/auth/forcedotcom', passport.authenticate('forcedotcom'), function(req,
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/forcedotcom/callback', passport.authenticate('forcedotcom', {
-    failureRedirect: '/login'
+  failureRedirect: '/login'
 }), function(req, res) {
-    res.redirect('/');
+  res.redirect('/');
 });
 
 app.get('/logout', function(req, res) {
-    res.redirect('/login');
+  res.redirect('/login');
 });
 
 app.listen(3000);
@@ -158,8 +162,8 @@ app.listen(3000);
 //   login page.
 
 function ensureAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
 }

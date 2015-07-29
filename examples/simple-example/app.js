e32c9b46e06c4b83a23b25112be3882f364e72f8
Fabian Jakobs <fabian.jakobs@web.de>
2015-07-29 14:58:11 +0000

    fix example. Latest version of express does not work.

--- examples/simple-example/app.js
+++ examples/simple-example/app.js
@@ -87,25 +87,23 @@ passport.use(sfStrategy);
 
 var app = express();
 
-// configure Express
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({
-    secret: 'keyboard cat'
-  }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
+// // configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.json());
+app.use(express.urlencoded());
+ //app.use(express.methodOverride());
+app.use(express.session({
+  secret: 'keyboard cat'
+}));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   console.log(req.user);

22952c9a691484b41fe4055db15be22be1ddafc5
Guy Maliar <gmaliar@users.noreply.github.com>
2022-02-13 18:46:14 +0200

    bump passport-oauth2 to 1.6.1

--- test/tests/connection.js
+++ test/tests/connection.js
@@ -1,9 +1,17 @@
-var http = require('http');
+var should = require('chai').should(),
+  http = require('http'),
+  app = require('../app').app;
 
 describe('Testing a simple app',function(){
+  var server;
   var response = {};
+
+  before(function () {
+    server = app.listen();
+  });
+
   before(function(done){
-    var req = http.get("http://localhost:3000/auth/forcedotcom", function(res) {
+    var req = http.get( 'http://localhost:' + server.address().port + '/auth/forcedotcom', function(res) {
       response.res = res;
       res.on('data', function (chunk) {
         response.body += chunk;
@@ -19,5 +27,9 @@ describe('Testing a simple app',function(){
       response.res.should.have.property('statusCode').to.equal(302);
       done();
     });
+  });
+
+  after(function () {
+    server.close();
   });
 });

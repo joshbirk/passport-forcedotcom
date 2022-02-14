383ca7d25be3c0b4005a2f7b06840454bdffc514
Guy Maliar <gmaliar@users.noreply.github.com>
2022-02-14 14:08:08 +0200

    create gluework fork

--- test/tests/connection.js
+++ test/tests/connection.js
@@ -1,8 +1,8 @@
-var should = require('chai').should(),
-  http = require('http'),
-  app = require('../app').app;
+var should = require("chai").should(),
+  http = require("http"),
+  app = require("../app").app;
 
-describe('Testing a simple app',function(){
+describe("Testing a simple app", function () {
   var server;
   var response = {};
 
@@ -10,21 +10,24 @@ describe('Testing a simple app',function(){
     server = app.listen();
   });
 
-  before(function(done){
-    var req = http.get( 'http://localhost:' + server.address().port + '/auth/forcedotcom', function(res) {
-      response.res = res;
-      res.on('data', function (chunk) {
-        response.body += chunk;
-      });
-      res.on('end', function () {
-        done();
-      });
-    });
+  before(function (done) {
+    var req = http.get(
+      "http://localhost:" + server.address().port + "/auth/forcedotcom",
+      function (res) {
+        response.res = res;
+        res.on("data", function (chunk) {
+          response.body += chunk;
+        });
+        res.on("end", function () {
+          done();
+        });
+      }
+    );
   });
 
-  describe('Express should respond',function(){
-    it('with a redirect',function(done){
-      response.res.should.have.property('statusCode').to.equal(302);
+  describe("Express should respond", function () {
+    it("with a redirect", function (done) {
+      response.res.should.have.property("statusCode").to.equal(302);
       done();
     });
   });
